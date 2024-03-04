67bb21182116a208a9a225fbad4d773af2e0013b
HarllonCS <harllon.santana@gmail.com>
2024-03-03 22:48:58 -0300

    display function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Get elements
+const card = document.querySelectorAll('.card'),
+    backLink = document.querySelectorAll('.back-link'),
+
+    sec = document.querySelector('section')
+
+card[0].addEventListener('click', () => {
+    sec.style.display = 'none'
+    table.style.display = 'block'
+})
+
+backLink[0].addEventListener('click', () => {
+    sec.style.display = 'block'
+    table.style.display = 'none'
+})
--- js/table.js
+++ js/table.js
@@ -1,61 +1,99 @@
+// Global variables to inputs
 const start = document.querySelector('#start'),
     end = document.querySelector('#end'),
-    num = document.querySelector('#number'),
-    steps = document.querySelector('#steps'),
-
+    // Get elements
     table = document.querySelector('#table'),
-    res = document.createElement('div'),
-    op = document.querySelectorAll('.table-op')
+    op = document.querySelectorAll('.table-op'),
+    // Create elements
+    tableRes = document.createElement('div')
 
-// Table function
+// Atribute the class "table-result"
+tableRes.className = 'table-result'
+
+// TABLE FUNCTION
 document.querySelectorAll('.btn')[0].addEventListener('click', () => {
     let s = Number(start.value),
         e = Number(end.value),
-        n = Number(num.value),
-        st = Number(steps.value)
+        steps = Number(document.querySelector('#steps').value)
 
-    res.style.marginTop = '2vw'
-    res.innerHTML = ''
+    // Clean result space
+    tableRes.innerHTML = ''
+    
+    // Call functions according to the condition
+    // s <= e ? increasingTable(s, e, steps) : decreasingTable(s, e, steps)
 
-    // Increasing Table
-    if (s <= e) {
-        if (op[0].checked) {
-            for (; s <= e; s += st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} + ${n} = ${(s + n).toFixed(2)}</p>`
-            }   
-        } else if (op[1].checked) {
-            for (; s <= e; s += st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} - ${n} = ${(s - n).toFixed(2)}</p>`
-            }
-        } else if (op[2].checked) {
-            for (; s <= e; s += st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} X ${n} = ${(s * n).toFixed(2)}</p>`
-            }
-        } else {
-            for (; s <= e; s += st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} ÷ ${n} = ${(s / n).toFixed(2)}</p>`
-            }
-        }
-    // Decreasing table
-    } else {
-        if (op[0].checked) {
-            for (; s >= e; s -= st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} + ${n} = ${(s + n).toFixed(2)}</p>`
-            }   
-        } else if (op[1].checked) {
-            for (; s >= e; s -= st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} - ${n} = ${(s - n).toFixed(2)}</p>`
-            }
-        } else if (op[2].checked) {
-            for (; s >= e; s -= st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} X ${n} = ${(s * n).toFixed(2)}</p>`
-            }
-        } else {
-            for (; s >= e; s -= st) {
-                res.innerHTML += `<p class='fw-bold text-center'>${s} ÷ ${n} = ${(s / n).toFixed(2)}</p>`
-            }
-        }
+    if (start.value.length == 0) {
+        tableRes.innerHTML = '<p class=\'fw-bold text-danger text-center\'>The start field must not be empty.</p>'
     }
 
-    table.append(res)
+    // console.log();
+
+    table.append(tableRes)
 })
+
+// Increasing Table
+function increasingTable(start, end, steps) {
+    // Generate table according to the string params
+    if (op[0].checked) {
+        for (; start <= end; start += steps) {
+            tableRes.innerHTML += generateTable('add')
+        }   
+    } else if (op[1].checked) {
+        for (; start <= end; start += steps) {
+            tableRes.innerHTML += generateTable('sub')
+        }
+    } else if (op[2].checked) {
+        for (; start <= end; start += steps) {
+            tableRes.innerHTML += generateTable('mult')
+        }
+    } else {
+        for (; start <= end; start += steps) {
+            tableRes.innerHTML += generateTable('div')
+        }
+    }
+}
+
+// Decreasing table
+function decreasingTable(start, end, steps) {
+    // Generate table according to the string params
+    if (op[0].checked) {
+        for (; start >= end; start -= steps) {
+            tableRes.innerHTML += generateTable('add')
+        }   
+    } else if (op[1].checked) {
+        for (; start >= end; start -= steps) {
+            tableRes.innerHTML += generateTable('sub')
+        }
+    } else if (op[2].checked) {
+        for (; start >= end; start -= steps) {
+            tableRes.innerHTML += generateTable('mult')
+        }
+    } else {
+        for (; start >= end; start -= steps) {
+            tableRes.innerHTML += generateTable('div')
+        }
+    }
+}
+
+// Generate table
+function generateTable(sign) {
+    let s = Number(start.value),
+        n = Number(document.querySelector('#number').value)
+
+    switch (sign) {
+        case 'add':
+            result = `<p class='fw-bold text-center'>${s} + ${n} = ${(s + n).toFixed(2)}</p>`
+            break
+        case 'sub':
+            result = `<p class='fw-bold text-center'>${s} - ${n} = ${(s - n).toFixed(2)}</p>`
+            break
+        case 'mult':
+            result = `<p class='fw-bold text-center'>${s} X ${n} = ${(s * n).toFixed(2)}</p>`
+            break
+        case 'div': 
+            result = `<p class='fw-bold text-center'>${s} ÷ ${n} = ${(s / n).toFixed(2)}</p>`
+        break
+    }
+
+    return result
+}
