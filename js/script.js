6aa081c6a013bd0877fc10010fcea15157e26cdc
HarllonCS <harllon.santana@gmail.com>
2024-03-12 10:32:02 -0300

    class

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Get elements
-const card = document.querySelectorAll('.card'),
-    backLink = document.querySelectorAll('.back-link'),
-    table = document.querySelector('#table'),
-    sec = document.querySelector('section')
+const card = document.querySelectorAll('.card')
+const backLink = document.querySelectorAll('.back-link')
+const table = document.querySelector('#table')
+const sec = document.querySelector('section')
 
+// Calculation Table Generator functions
 card[0].addEventListener('click', () => {
     sec.style.display = 'none'
     table.style.display = 'block'
 })
-
 backLink[0].addEventListener('click', () => {
     sec.style.display = 'block'
     table.style.display = 'none'
--- js/table.js
+++ js/table.js
@@ -1,19 +1,19 @@
 // Global inputs variables
-const start = document.querySelector('#start'),
-    end = document.querySelector('#end'),
-    // Get elements
-    op = document.querySelectorAll('.table-op'),
-    // Create elements
-    tableRes = document.createElement('div')
+const start = document.querySelector('#start')
+const end = document.querySelector('#end')
+// Get elements
+const op = document.querySelectorAll('.table-op')
+// Create elements
+const tableRes = document.createElement('div')
 
 // Atribute the class "table-result"
 tableRes.className = 'table-result'
 
 // TABLE FUNCTION
 document.querySelectorAll('.btn')[0].addEventListener('click', () => {
-    let s = Number(start.value),
-        e = Number(end.value),
-        steps = Number(document.querySelector('#steps').value)
+    let s = Number(start.value)
+    let e = Number(end.value)
+    let steps = Number(document.querySelector('#steps').value)
 
     // Clean result space
     tableRes.innerHTML = ''
@@ -33,7 +33,6 @@ function increasingTable(start, end, steps) {
     if (op[0].checked) {
         for (; start <= end; start += steps) {
             tableRes.innerHTML += generateTable(start, 'add')
-            // console.log(start);
         }   
     } else if (op[1].checked) {
         for (; start <= end; start += steps) {
@@ -74,22 +73,21 @@ function decreasingTable(start, end, steps) {
 
 // Generate table
 function generateTable(s, operation) {
-    const n = Number(document.querySelector('#number').value)
+    const n = Number(document.querySelector('#number').value).toFixed(2)
 
     switch (operation) {
         case 'add':
-            result = `<p class='fw-bold text-center'>${s} + ${n} = ${(s + n).toFixed(2)}</p>`
+            result = `<p class='fw-bold text-center'>${s.toFixed(2)} + ${n} = ${(s + n).toFixed(2)}</p>`
             break
         case 'sub':
-            result = `<p class='fw-bold text-center'>${s} - ${n} = ${(s - n).toFixed(2)}</p>`
+            result = `<p class='fw-bold text-center'>${s.toFixed(2)} - ${n} = ${(s - n).toFixed(2)}</p>`
             break
         case 'mult':
-            result = `<p class='fw-bold text-center'>${s} X ${n} = ${(s * n).toFixed(2)}</p>`
+            result = `<p class='fw-bold text-center'>${s.toFixed(2)} X ${n} = ${(s * n).toFixed(2)}</p>`
             break
         case 'div': 
-            result = `<p class='fw-bold text-center'>${s} ÷ ${n} = ${(s / n).toFixed(2)}</p>`
+            result = `<p class='fw-bold text-center'>${s.toFixed(2)} ÷ ${n} = ${(s / n).toFixed(2)}</p>`
         break
     }
-
     return result
 }
