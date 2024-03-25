4d2f46244a3258cdcc7ce2e31f50eb513c1c36a0
HarllonCS <harllon.santana@gmail.com>
2024-03-24 22:59:03 -0300

    Temeperature Calculator

--- js/script.js
+++ js/script.js
@@ -5,21 +5,31 @@ const box = document.querySelectorAll('.input-box')
 const sec = document.querySelector('section')
 
 // Calculation Table Generator functions
-card[0].addEventListener('click', () => {
+card[0].onclick = () => {
     sec.style.display = 'none'
     box[0].style.display = 'block'
-})
-backLink[0].addEventListener('click', () => {
+}
+backLink[0].onclick = () => {
     sec.style.display = 'block'
     box[0].style.display = 'none'
-})
+}
 
 // Unit Converter functions
-card[1].addEventListener('click', () => {
+card[1].onclick = () => {
     sec.style.display = 'none'
     box[1].style.display = 'block'
-})
-backLink[1].addEventListener('click', () => {
+}
+backLink[1].onclick = () => {
     sec.style.display = 'block'
     box[1].style.display = 'none'
-})
+}
+
+// Temperature functions
+card[2].onclick = () => {
+    sec.style.display = 'none'
+    box[2].style.display = 'block'
+}
+backLink[2].onclick = () => {
+    sec.style.display = 'block'
+    box[2].style.display = 'none'
+}
--- js/temperature.js
+++ js/temperature.js
@@ -1,19 +1,69 @@
+// Create elements
 const tempResult = document.createElement('div')
 tempResult.className = 'result'
-
+// Get elements
 const temperature = document.getElementById('temperature')
 const tempBtn = document.querySelectorAll('.btn')[2]
+const selectTemp = document.querySelector('#temperature select')
+const radioTemp = document.querySelectorAll('#temperature .form-check-input')
 
+// TEMPERATURE function
 tempBtn.onclick = function() {
+    const errorMsg = 'Invalid Operation!'
     const num = Number(document.getElementById('tempNumber').value)
-    
-    const celsiusfahrenheit = 9/5 * num + 32
-    const celsiusKelvin = 0
-    
-    const kelvin = 0
+    // Celsius formulas
+    const celsiusFahrenheit = 9/5 * num + 32
+    const celsiusKelvin = num + 273.15
+    // Fahrenheit formulas
+    const fahrenheitCelsius = (num - 32) * 5/9
+    const fahrenheitKelvin = fahrenheitCelsius + 273.15
+    // Kelvin formulas
+    const kelvinCelsius = num - 273.15
+    const kelvinFahrenheit = kelvinCelsius * 9/5 + 32
+    // Initialize the variable to get the result
+    let result = null
 
-    tempResult.innerHTML = `<p class="text-center fw-bold">${num}°C = ${celsiusfahrenheit}°F</p>`
+    // Fahrenheit conversion
+    if (selectTemp.value == 'fahrenheit') {
+        result = `${num}°F = `
+        // Fahrenheit to celsius
+        if (radioTemp[1].checked) {
+            result += `${fahrenheitCelsius.toFixed(2)}°C`
+        // Fahrenheit to kelvin
+        } else if (radioTemp[2].checked) {
+            result += `${fahrenheitKelvin.toFixed(2)}K`
+        // Fahrenheit to itself
+        } else {
+            result = `<span class="text-danger">[ERRO]: ${errorMsg}</span>`
+        }
+    // Celsius conversion
+    } else if (selectTemp.value == 'celsius') {
+        result = `${num}°C = `
+        // Celsius to fahrenheit
+        if (radioTemp[0].checked) {
+            result += `${celsiusFahrenheit.toFixed(2)}°F`
+        // Celsius to kelvin
+        } else if (radioTemp[2].checked) {
+            result += `${celsiusKelvin.toFixed(2)}K`
+        // Celsius to itself
+        } else {
+            result = `<span class="text-danger">[ERRO]: ${errorMsg}</span>`
+        }
+    // Kelvin conversion
+    } else {
+        result = `${num}K = `
+        // Kelvin to celsius
+        if (radioTemp[0].checked) {
+            result += `${kelvinFahrenheit.toFixed(2)}°F`
+        // Kelvin to celsius
+        } else if (radioTemp[1].checked) {
+            result += `${kelvinCelsius.toFixed(2)}°C`
+        // Kelvin to itself
+        } else {
+            result = `<span class="text-danger">[ERRO]: ${errorMsg}</span>`
+        }
+    }
+    tempResult.innerHTML = `<p class="text-center fw-bold">${result}</p>`
 
     temperature.append(tempResult)
-
 }
